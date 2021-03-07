6c5ee16cc0f40535112fe20f969588df766ebc5c
Luke Bonner <bonnerlu@mail.gvsu.edu>
2021-03-06 21:24:04 -0500

    NEW SHIT

--- dist/main.c39d6dcf.js
+++ dist/main.c39d6dcf.js
@@ -1937,12 +1937,55 @@ var thebutton;
 theInput = document.querySelector("#userInput > input[type=text]");
 thebutton = document.querySelector("#userInput > button");
 var breedLimit = Number(theInput === null || theInput === void 0 ? void 0 : theInput.value);
+var tableBody;
+tableBody = document.querySelector("#progOutput table > tbody");
 console.log(thebutton);
 thebutton === null || thebutton === void 0 ? void 0 : thebutton.addEventListener("click", function () {
-  var _a;
+  var oldRows;
+  oldRows = tableBody === null || tableBody === void 0 ? void 0 : tableBody.querySelectorAll("tr");
 
-  var inputLen = (_a = theInput === null || theInput === void 0 ? void 0 : theInput.value.length) !== null && _a !== void 0 ? _a : 0;
-  if (inputLen > 0) console.log("You entered", theInput === null || theInput === void 0 ? void 0 : theInput.value);else console.log("Please enter some text");
+  if (oldRows) {
+    for (var k = 0; k < (oldRows === null || oldRows === void 0 ? void 0 : oldRows.length); k++) {
+      var oldOne = oldRows[k];
+      tableBody === null || tableBody === void 0 ? void 0 : tableBody.removeChild(oldOne);
+    }
+  }
+
+  axios_1.default.get(catURL, {
+    params: {
+      limit: breedLimit
+    }
+  }).then(function (r) {
+    return r.data;
+  }).then(function (breeds) {
+    var meowArr = breeds.map(function (b) {
+      return {
+        name: b.name,
+        description: b.description,
+        image: b.image
+      };
+    });
+    return meowArr;
+  }).then(function (meows) {
+    var tableBody;
+    tableBody = document.querySelector("#progOutput table > tbody");
+
+    for (var i = 0; i < breedLimit; i++) {
+      var catImage = document.createElement("img");
+      catImage.setAttribute("src", meows[i].image.url);
+      var tr = document.createElement("tr");
+      var meowNameCell = document.createElement("td");
+      meowNameCell.appendChild(document.createTextNode(meows[i].name));
+      var meowDescCell = document.createElement("td");
+      meowDescCell.appendChild(document.createTextNode(meows[i].description));
+      var meowImagCell = document.createElement("td");
+      meowImagCell.appendChild(catImage);
+      tr.appendChild(meowNameCell);
+      tr.appendChild(meowDescCell);
+      tr.appendChild(meowImagCell);
+      tableBody === null || tableBody === void 0 ? void 0 : tableBody.appendChild(tr);
+    }
+  });
 });
 if (isNaN(breedLimit)) breedLimit = 10;
 var catURL = "https://api.thecatapi.com/v1/breeds";
@@ -1980,6 +2023,18 @@ axios_1.default.get(catURL, {
     tableBody === null || tableBody === void 0 ? void 0 : tableBody.appendChild(tr);
   });
 });
+axios_1.default.get("https://dog.ceo/api/breed/hound/images").then(function (r) {
+  return r.data;
+}).then(function (dogPics) {
+  var tableBody;
+  tableBody = document.querySelector("#progOutputDog table > tbody");
+
+  for (var i = 0; i < 10; i++) {
+    var dogImage = document.createElement("img");
+    dogImage.setAttribute("src", dogPics.message[i]);
+    tableBody === null || tableBody === void 0 ? void 0 : tableBody.appendChild(dogImage);
+  }
+});
 },{"axios":"../node_modules/axios/index.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -2008,7 +2063,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "11671" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "10443" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/main.ts
+++ src/main.ts
@@ -23,14 +23,65 @@ let thebutton: HTMLButtonElement | null;
 theInput = document.querySelector("#userInput > input[type=text]");
 thebutton = document.querySelector("#userInput > button");
 let breedLimit = Number(theInput?.value);
+let tableBody: HTMLTableSectionElement | null;
+  tableBody = document.querySelector("#progOutput table > tbody");
 console.log(thebutton);
 thebutton?.addEventListener("click", () => {
-  const inputLen = theInput?.value.length ?? 0;
-  if (inputLen > 0)  
-    console.log("You entered", theInput?.value);
-  else
-    console.log("Please enter some text");
-});
+  let oldRows: NodeListOf<HTMLTableRowElement> | undefined;
+  oldRows = tableBody?.querySelectorAll("tr");
+  if (oldRows) {
+    for (let k = 0; k < oldRows?.length; k++) {
+      const oldOne = oldRows[k];
+      tableBody?.removeChild(oldOne);
+    }
+  }
+
+  axios
+.get(catURL, {
+  params: {
+    limit: breedLimit,
+  },
+})
+.then((r: AxiosResponse) => r.data)
+.then((breeds: any[]) => {
+  const meowArr = breeds.map(
+    (b: any): Meow =>{
+      return{
+        name: b.name,
+        description: b.description,
+        image: b.image,
+
+      };
+    }
+  );
+  return meowArr;
+})
+.then((meows: Meow[]) => {
+  let tableBody: HTMLTableSectionElement | null;
+  tableBody = document.querySelector("#progOutput table > tbody");
+  for(let i = 0;i<breedLimit;i++){
+    const catImage: HTMLImageElement = document.createElement("img");
+    catImage.setAttribute("src",meows[i].image.url);
+    const tr = document.createElement("tr");
+    const meowNameCell = document.createElement("td");
+    meowNameCell.appendChild(document.createTextNode(meows[i].name))
+    const meowDescCell = document.createElement("td");
+    meowDescCell.appendChild(document.createTextNode(meows[i].description))
+    const meowImagCell = document.createElement("td");
+    meowImagCell.appendChild(catImage)
+    tr.appendChild(meowNameCell);
+    tr.appendChild(meowDescCell);
+    tr.appendChild(meowImagCell);
+
+    tableBody?.appendChild(tr);
+
+
+  }
+
+  })
+  
+})
+
 
 if(isNaN(breedLimit)) breedLimit = 10;
 const catURL = "https://api.thecatapi.com/v1/breeds";
@@ -77,4 +128,22 @@ axios
 
 
   })
+  
 })
+
+axios.get("https://dog.ceo/api/breed/hound/images")
+.then((r: AxiosResponse) => r.data)
+.then((dogPics: any)=>{
+  let tableBody: HTMLTableSectionElement | null;
+  tableBody = document.querySelector("#progOutputDog table > tbody");
+  for(let i = 0;i<10;i++){
+  const dogImage: HTMLImageElement = document.createElement("img");
+  
+  dogImage.setAttribute("src",dogPics.message[i]);
+  tableBody?.appendChild(dogImage);
+  }
+  
+})
+
+
+
